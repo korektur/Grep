8b9bad4d51ab52b62c1f52551e264fdc51548802
Ruslan <akhundov2@gmail.com>
2014-02-15 12:51:23 +0400

    upd

--- src/ru/ifmo/ctddev/Akhundov/task1/Grep.java
+++ src/ru/ifmo/ctddev/Akhundov/task1/Grep.java
@@ -1,16 +1,49 @@
 package ru.ifmo.ctddev.Akhundov.task1;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Grep {
 
-    public static void main(String[] args){
-        if (args.length == 1){
-            System.out.print("Wrong parameters");
-        } else if (args.length == 2 && "-".equals(args[1])){
-            try {
-                Reader in = new BufferedReader(new InputStreamReader(System.in));
+    private static ArrayList<String> stringsToFind;
+
+    private static void checkFile(File f){
+
+    }
+
+
+    private static void bypassFiles(File file) {
+        File[] listOfFiles = file.listFiles();
+        if (listOfFiles == null) {
+            return;
+        }
+        for(File f: listOfFiles){
+            if (f.isDirectory()){
+                bypassFiles(f);
+            } else {
+                checkFile(f);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        stringsToFind = new ArrayList<String>();
+        if (args.length == 0) {
+            System.out.print("Wrong parameters");
+        } else if (args.length == 1 && "-".equals(args[1])) {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                String[] input = in.readLine().split(" ");
+                Collections.addAll(stringsToFind, input);
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Collections.addAll(stringsToFind, args);
+        }
+        File f = new File(Grep.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        bypassFiles(f);
     }
 }
